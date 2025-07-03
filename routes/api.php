495cde9abca0ad5041993ac0529d096e2975409c
Luke Sun <luke@soft.pw>
2025-07-03 21:57:12 +0700

    code style refactoring

--- routes/api.php
+++ routes/api.php
@@ -13,10 +13,12 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::post('/screen', [KycScreenerController::class, 'screen'])->middleware(VerifyApiKey::class);
-Route::post('/e-form-kyb', [KybScreenController::class, 'kyb'])->middleware(VerifyApiKey::class);
-Route::post('/e-form-onboarding', [EntityOnboardingController::class, 'check'])->middleware(VerifyApiKey::class);
-Route::get('/status/{uuid}', [KycScreenerController::class, 'status'])->middleware(VerifyApiKey::class);
+Route::middleware(VerifyApiKey::class)->group(function () {
+    Route::post('/screen', [KycScreenerController::class, 'screen']);
+    Route::get('/e-form-kyb', [KybScreenController::class, 'kyb']);
+    Route::get('/e-form-onboarding', [EntityOnboardingController::class, 'check']);
+    Route::get('/status/{uuid}', [KycScreenerController::class, 'status']);
+});
 
 Route::prefix('v1')->group(function (){
     Route::post('/ocr', [KycController::class, 'ocr'])->middleware(VerifyJwtMiddleware::class);
