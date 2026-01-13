2228b03da1d8257cdde982974c898dec9f040968
Luke Sun <luke@soft.pw>
2026-01-13 12:36:48 +0700

    fix: add djkyc exclude from csrf

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -18,6 +18,7 @@ return Application::configure(basePath: dirname(__DIR__))
             'kyc',
             'liveness',
             'djkyb',
+            'djkyc',
         ]);
 
         $middleware->alias([
